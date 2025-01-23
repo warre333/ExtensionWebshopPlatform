eb45d284066bd426cfa95728a5bb15bb862f72bc
warre002 <warre.bossaert@gmail.com>
2025-01-23 21:50:38 +0100

    Orders on dashboard UI

--- BasePlatform/base-platform/app/dashboard/page.tsx
+++ BasePlatform/base-platform/app/dashboard/page.tsx
@@ -14,20 +14,48 @@ export default async function DashboardPage() {
   }
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-12">
-      {/* <div className="w-full">
-        <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
-          <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated
-          user
+    <div className="flex-1 w-full flex flex-col gap-4">
+      <div className="flex items-center gap-4">
+        <h1 className="text-3xl font-medium">Dashboard</h1>
+      </div>
+      <div className="flex flex-col gap-8">
+        <div className="flex items-center gap-4">
+          <div className="w-1/2">
+            <h2 className="text-lg font-light">Welcome back, {user.email}!</h2>
+          </div>          
         </div>
-      </div> */}
-      {/* <div className="flex flex-col gap-2 items-start">
-        <h2 className="font-bold text-2xl mb-4">Your user details</h2>
-        <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
-          {JSON.stringify(user, null, 2)}
-        </pre>
-      </div> */}
+        <div className="flex flex-col gap-4">
+          <h1 className="text-xl font-medium">Orders</h1>
+          <table className="min-w-full divide-y divide-gray-200 border border-gray-200">
+            <thead className="bg-gray-100">
+              <tr>
+                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Order ID
+                </th>
+                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Customer name
+                </th>
+                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Order date
+                </th>
+                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Status
+                </th>
+                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  View order
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              <tr>
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">Data 1</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Data 2</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Data 3</td>
+              </tr>              
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
